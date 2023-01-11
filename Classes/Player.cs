b4dc7c8d49a1b33b0bcadaaa6eb681c390ba9846
Cody Washbrook <codywashbrook@hotmail.com>
2023-01-11 09:42:53 -0400

    new classes (player, enemy)

---
+++ Classes/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Player
+    {
+        public int x;
+        public int y;
+        public int health;
+        public string name;
+
+        public Player(string name, int health)
+        {
+            x = 0;
+            y = 0;
+            this.health = health;
+            this.name = name; //use for different gamertags for example
+            Console.WriteLine("Player class constructed");
+        }
+
+        public void TakeDamage(int hp)
+        {
+            health -= hp;
+
+            Console.WriteLine("Player has taken .. " + hp + " damage");
+
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine(name + " Health: " + health);
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -8,10 +8,32 @@ namespace Classes
 {
     internal class Program
     {
+        struct Vector2
+        {
+            public int x;
+            public int y;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Classes: ");
             Console.WriteLine();
+
+            //Player player = new Player();
+            Player player; //declaration
+            player  = new Player("Cody", 100); //instantiation
+            player.health = 100;
+            player.ShowStats();
+            //Console.WriteLine(player.name + " health = " + player.health);
+            player.TakeDamage(15);
+            player.ShowStats();
+            //Console.WriteLine(player.name + " health = " + player.health);
+
+
+            Vector2 position;
+
+
+            Enemy enemy = new Enemy();
 
             Console.ReadKey(true);
         }
